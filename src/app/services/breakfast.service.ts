7c4296fce4fa0d5e3f3b6cca76c4b6df8741eb87
Maddie Ijams <maddieijams@gmail.com>
2019-06-19 12:23:14 -0400

    adjusted api urls and package.json

--- src/app/services/breakfast.service.ts
+++ src/app/services/breakfast.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { APIURL } from '../../environments/environment.prod';
+import { APIURL } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -12,28 +12,28 @@ export class BreakfastService {
 
   // Get all breakfast items
   getAllBreakfast() {
-    return this.http.get(`http://localhost:4000/breakfast/all`);
+    return this.http.get(`${APIURL}/breakfast/all`);
   }
 
   // Create breakfast item
   createBreakfast(form) {
     JSON.stringify(form);
-    return this.http.post(`http://localhost:4000/breakfast/create`, form);
+    return this.http.post(`${APIURL}/breakfast/create`, form);
   }
 
   // Get single breakfast by id for updating
   getBfastItem(id) {
-    return this.http.get(`http://localhost:4000/breakfast/one/${id}`);
+    return this.http.get(`${APIURL}/breakfast/one/${id}`);
   }
 
   // Update breakfast by id
   updateBreakfast(id, form) {
     console.log(form);
-    return this.http.put(`http://localhost:4000/breakfast/edit/${id}`, form);
+    return this.http.put(`${APIURL}/breakfast/edit/${id}`, form);
   }
 
   // Delete breakfast by id
   deleteLandmark(id) {
-    return this.http.delete(`http://localhost:4000/breakfast/delete/${id}`);
+    return this.http.delete(`${APIURL}/breakfast/delete/${id}`);
   }
 }
