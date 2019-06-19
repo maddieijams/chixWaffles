d4f9214a8b7213490a3ecc8215400dd3c0eb88b8
Maddie Ijams <maddieijams@gmail.com>
2019-06-19 12:43:58 -0400

    dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ switch (window.location.hostname) {
     // this is the deployed angular application
     case 'chix-waffle-client.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://chix-waffle-client.herokuapp.com'
+        APIURL = 'https://chix-waffle-client.herokuapp.com';
         break;
     default:
         // this is the local host name of your API
